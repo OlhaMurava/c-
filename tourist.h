5ac0a973b28d5fad98f7c65ab7039368bfe49ce2
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 17:58:11 +0300

    Update tourist.h

--- tourist.h
+++ tourist.h
@@ -5,7 +5,7 @@
 
 namespace objects {
     class Tourist {
-    public:
+    protected:        // записуємо змінні у захищеному доступі, щоб потім можна було застосувати успадкування
         std::string name;
         int age;
         int gender;
@@ -13,20 +13,18 @@ namespace objects {
         std::string category;
         double data;
         double enddata;
-        std::string places;
         std::string luggage;
-        std:: string time;
+        std::string time;
         int race;
         std::string tag;
         std::string marking;
         int rooms;
         int visits;
         std::string agency;
-        bool usedExcurions;
         int group;
         double losses;
         double mass;
-
+    public: // публічний доступ
         Tourist(std::string name,
             const int age,
             const int gender,
@@ -34,7 +32,6 @@ namespace objects {
             std::string category,
             const double data,
             const double enddata,
-            std::string places,
             std::string luggage,
             std::string time,
             const int race,
@@ -43,14 +40,55 @@ namespace objects {
             const int rooms,
             const int visits,
             std::string agency,
-            const bool usedExcurions,
             const int group,
             const double losses,
             const double mass);
 
-        void print(const bool param = false) const;
-        void printHotel() const;
-        void printFinances() const;
-        void printRaceInfo() const;
+        virtual ~Tourist(); 
+        // Setters
+        void setName(const std::string& name);
+        void setAge(int age);
+        void setGender(int gender);
+        void setHotel(const std::string& hotel);
+        void setData(double data);
+        void setEndData(double enddata);
+        void setLuggage(const std::string& luggage);
+        void setTime(const std::string& time);
+        void setRace(int race);
+        void setTag(const std::string& tag);
+        void setMarking(const std::string& marking);
+        void setRooms(int rooms);
+        void setVisits(int visits);
+        void setAgency(const std::string& agency);
+        void setGroup(int group);
+        void setLosses(double losses);
+        void setMass(double mass);
+        void setCategory(const std::string& category);
+
+        // Getters
+        std::string getName() const;
+        int getAge() const;
+        int getGender() const;
+        std::string getHotel() const;
+        double getData() const;
+        double getEndData() const;
+        std::string getLuggage() const;
+        std::string getTime() const;
+        int getRace() const;
+        std::string getTag() const;
+        std::string getMarking() const;
+        int getRooms() const;
+        int getVisits() const;
+        std::string getAgency() const;
+        int getGroup() const;
+        double getLosses() const;
+        double getMass() const;
+        std::string getCategory() const;
+// Віртуальні функції, поліморфізм
+        virtual void print(const bool param = false) const = 0;
+        virtual void printHotel() const = 0;
+        virtual void printFinances() const = 0;
+        virtual void printRaceInfo() const = 0;
+
     };
 }
