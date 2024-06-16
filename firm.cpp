04655958d57a7a55738b3ba9f9c9b32a1468a233
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 18:07:41 +0300

    Update firm.cpp

--- firm.cpp
+++ firm.cpp
@@ -14,7 +14,7 @@ Firm::Firm(
     const double mass_food,
     const double mass_tourist,
     std::string hour,
-    std::string rentability):
+    std::string name) :
     plane_race(plane_race),
     income(income),
     losses(losses),
@@ -25,7 +25,31 @@ Firm::Firm(
     mass_food(mass_food),
     mass_tourist(mass_tourist),
     hour(move(hour)),
-    rentability(move(rentability)){}
+    name(move(name)) {}
+
+void Firm::setPlaneRace(int plane_race) { this->plane_race = plane_race; }
+void Firm::setIncome(double income) { this->income = income; }
+void Firm::setLosses(double losses) { this->losses = losses; }
+void Firm::setLossesExcursions(double losses_excursions) { this->losses_excursions = losses_excursions; }
+void Firm::setLossesHotels(double losses_hotels) { this->losses_hotels = losses_hotels; }
+void Firm::setLossesPlanes(double losses_planes) { this->losses_planes = losses_planes; }
+void Firm::setMassTech(double mass_tech) { this->mass_tech = mass_tech; }
+void Firm::setMassFood(double mass_food) { this->mass_food = mass_food; }
+void Firm::setMassTourist(double mass_tourist) { this->mass_tourist = mass_tourist; }
+void Firm::setHour(const std::string& hour) { this->hour = hour; }
+void Firm::setRentability(const std::string& rentability) { this->name = name; }
+
+int Firm::getPlaneRace() const { return plane_race; }
+double Firm::getIncome() const { return income; }
+double Firm::getLosses() const { return losses; }
+double Firm::getLossesExcursions() const { return losses_excursions; }
+double Firm::getLossesHotels() const { return losses_hotels; }
+double Firm::getLossesPlanes() const { return losses_planes; }
+double Firm::getMassTech() const { return mass_tech; }
+double Firm::getMassFood() const { return mass_food; }
+double Firm::getMassTourist() const { return mass_tourist; }
+std::string Firm::getHour() const { return hour; }
+std::string Firm::getName() const { return name; }
 
 void Firm::printRentability() const {
     cout << fixed << setprecision(2);
@@ -76,3 +100,4 @@ void Firm::printCargo() const {
     cout << mass_tech / mass << endl;
     cout << "< ------------------------ >" << endl;
 }
+
