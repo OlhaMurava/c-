7acc78bffdfca083c4f89464fa2fed35852c0a73
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 18:03:35 +0300

    Update plane.h

--- plane.h
+++ plane.h
@@ -4,16 +4,30 @@
 
 namespace objects {
     class Plane {
-    public:
+    private:     // приватна зона доступу, змінні прописані всередині класу, інкапсуляція
         int number;
         double mass, volume;
         int passengers;
-
+    public:
         Plane(const int number,
             const double mass,
             const double volume,
             const int passengers);
 
+        // Setters
+        void setNumber(int number);
+        void setMass(double mass);
+        void setVolume(double volume);
+        void setPassengers(int passengers);
+
+        // Getters
+        int getNumber() const;
+        double getMass() const;
+        double getVolume() const;
+        int getPassengers() const;
+
+
         void print() const;
     };
 }
+
