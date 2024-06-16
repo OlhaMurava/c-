4a41892ea45b78f2e3e795846c07b1ca4a0de49e
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 18:06:05 +0300

    Update storage.cpp

--- storage.cpp
+++ storage.cpp
@@ -12,7 +12,21 @@ Storage::Storage(const double mass,
     planes_carier(planes_carier),
     planes_passenger(planes_passenger),
     planes(planes),
-    seats(seats){}
+    seats(seats) {}
+
+// Setters
+void Storage::setMass(double mass) { this->mass = mass; }
+void Storage::setPlanesCarrier(int planes_carier) { this->planes_carier = planes_carier; }
+void Storage::setPlanesPassenger(int planes_passenger) { this->planes_passenger = planes_passenger; }
+void Storage::setPlanes(int planes) { this->planes = planes; }
+void Storage::setSeats(int seats) { this->seats = seats; }
+
+// Getters
+double Storage::getMass() const { return mass; }
+int Storage::getPlanesCarrier() const { return planes_carier; }
+int Storage::getPlanesPassenger() const { return planes_passenger; }
+int Storage::getPlanes() const { return planes; }
+int Storage::getSeats() const { return seats; }
 
 void Storage::print() const {
     cout << "[< ------------------------- >]" << endl;
@@ -28,3 +42,5 @@ void Storage::print() const {
     cout << seats << endl;
     cout << "[< ------------------------- >]" << endl;
 }
+
+
