31a6138966a546c0b403e921edf6e085b8f26d5e
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 18:05:00 +0300

    Update storage.h

--- storage.h
+++ storage.h
@@ -3,17 +3,34 @@
 
 namespace objects {
     class Storage {
-    public:
+    private:       // приватна зона доступу, змінні оголошенні всередині класу, інкапсуляція
         double mass;
         int planes_carier, planes_passenger;
         int planes;
         int seats;
+    public:
         Storage(const double mass,
             const int planes_carier,
             const int planes_passenger,
             const int planes,
             const int seats);
 
+        // Setters
+        void setMass(double mass);
+        void setPlanesCarrier(int planes_carier);
+        void setPlanesPassenger(int planes_passenger);
+        void setPlanes(int planes);
+        void setSeats(int seats);
+
+        // Getters
+        double getMass() const;
+        int getPlanesCarrier() const;
+        int getPlanesPassenger() const;
+        int getPlanes() const;
+        int getSeats() const;
+
         void print() const;
     };
 }
+
+
