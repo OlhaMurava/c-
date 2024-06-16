6a39ada191c7985cf76064bda31eaa6c02da0c9e
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 18:01:58 +0300

    Update agency.h

--- agency.h
+++ agency.h
@@ -6,15 +6,28 @@
 
 namespace objects {
     class Agency {
-    public:
+    private:       // приватна зона доступу, змінні прописані всередині класу, інкапсуляція
         std::string name;
         int rating;
         std::string popular;
         std::vector<std::string> excursions;
-
+    public:
         Agency(std::string name,
             const int rating);
+
+        // Setters
+        void setName(const std::string& name);
+        void setRating(int rating);
+        void setPopular(const std::string& popular);
+        void setExcursions(const std::vector<std::string>& excursions);
+
+        // Getters
+        std::string getName() const;
+        int getRating() const;
+        std::string getPopular() const;
+        std::vector<std::string> getExcursions() const;
 
         void print() const;
     };
 }
+
