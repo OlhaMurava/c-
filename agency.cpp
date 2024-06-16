df377bef2c5988dde930272d2f566e5978fcb4d3
OlhaMurava <157117682+OlhaMurava@users.noreply.github.com>
2024-06-16 18:02:51 +0300

    Update agency.cpp

--- agency.cpp
+++ agency.cpp
@@ -8,6 +8,18 @@ Agency::Agency(string name,
     name(move(name)),
     rating(rating) {}
 
+// Setters
+void Agency::setName(const std::string& name) { this->name = name; }
+void Agency::setRating(int rating) { this->rating = rating; }
+void Agency::setPopular(const std::string& popular) { this->popular = popular; }
+void Agency::setExcursions(const std::vector<std::string>& excursions) { this->excursions = excursions; }
+
+// Getters
+std::string Agency::getName() const { return name; }
+int Agency::getRating() const { return rating; }
+std::string Agency::getPopular() const { return popular; }
+std::vector<std::string> Agency::getExcursions() const { return excursions; }
+// вивід
 void Agency::print() const {
     cout << "[< ------------------------- >]" << endl;
     cout << "Найякісніше екскурсійне турагенство: ";
